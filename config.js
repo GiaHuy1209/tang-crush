ffcefb64524c2779e117e18a93597a46497906bb
GiaHuy1209 <100330056+GiaHuy1209@users.noreply.github.com>
2022-02-24 11:13:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,7 @@ const CONFIG = {
     introTitle: 'Minh Hạnh ơi!',
     introDesc: `Anh biết chúng ta đã bỏ lỡ nhau rất nhiều lần
     Nhưng bây giờ đã có anh ở đây rồi   
-    Hãy cho anh cơ hội được quan tâm và bảo vệ em 
-    a
+    Hãy cho anh cơ hội được quan tâm và bảo vệ em
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
@@ -12,6 +11,5 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Em yêu anh <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    a
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘'
     messLink: https://www.facebook.com/profile.php?id=100032205961925
